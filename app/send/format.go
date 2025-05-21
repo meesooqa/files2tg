391346535a9753b1408bd59e7a34dd641ed8a597
meesooqa <meesooqa@hotmail.com>
2025-05-21 22:48:23 +0300

    minify default formatter

--- app/send/format.go
+++ app/send/format.go
@@ -1,9 +1,7 @@
 package send
 
 import (
-	"fmt"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"github.com/meesooqa/files2tg/app/finder"
@@ -13,30 +11,6 @@ type TelegramFormatter struct{}
 
 // Format generates HTML message from provided finder.File
 func (o *TelegramFormatter) Format(file finder.File) string {
-	// file.Name == "Секретная инфа [3873955765].mp4"
 	ext := filepath.Ext(file.Name)
-	base := strings.TrimSuffix(file.Name, ext)
-	re := regexp.MustCompile(`^(?P<name>.*)\s*\[(?P<id>\d+)\]$`)
-	var id, title string
-	matches := re.FindStringSubmatch(base)
-	if matches != nil {
-		result := map[string]string{}
-		for i, name := range re.SubexpNames() {
-			if i != 0 && name != "" {
-				result[name] = matches[i]
-			}
-		}
-		title = strings.TrimSpace(result["name"])
-		id = result["id"]
-	} else {
-		title = base
-	}
-	line1 := fmt.Sprintf("<code>%s</code> %s", id, title)
-
-	hashtag := "#gilticus"
-	// timeLayout := time.RFC3339
-	timeLayout := "2006-01-02 15:04:05"
-	line2 := fmt.Sprintf("<code>%s</code> %s", file.ModTime.Format(timeLayout), hashtag)
-
-	return line1 + "\n" + line2
+	return strings.TrimSuffix(file.Name, ext)
 }
